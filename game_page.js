be3237435dbd41270d358aa3f440ed4cd99d5e5f
oscorelpro <109703969+oscorelpro@users.noreply.github.com>
2022-11-30 19:00:00 -0600

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -14,3 +14,22 @@ document.getElementById("player2_score").innerHTML = player2_score;
 
 document.getElementById("player_question").innerHTML = "Turno para preguntar - " + player1_name;
 document.getElementById("player_answer").innerHTML = "Turno para responder - " + player2_name;
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+    console.log(actual_answer);
+
+
+    question_number = "<h4>" + number1 + " X "+ number2 + "</h4>";
+    input_box = "<br>Respuesta : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Comprobar</button>";
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
+
+
+question_turn = "player1";
+answer_turn = "player2";
